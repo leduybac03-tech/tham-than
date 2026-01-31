9aefc392639f6b68555b4c7b9a1b9714a0847955
unknown <leduybac03@gmail.com>
2026-01-31 09:32:29 +0700

    31/1-2

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 export const http = axios.create({
-  // baseURL: 'https://backend-tham-than.vercel.app/api/',
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://backend-tham-than.vercel.app/api/',
+  // baseURL: 'http://localhost:5000/api/',
   headers: {
     "Content-Type": "application/json",
-    // withCredentials: true,
+    withCredentials: true,
   },
 });
